d9bae52c61b4e355221d633bd7f43ccaa6dca55d
prateekgour <prateekgour99@gmail.com>
2022-03-20 00:44:22 +0530

    push RegisterPage with updates

--- src/main/java/pageObjects/RegisterPage.java
+++ src/main/java/pageObjects/RegisterPage.java
@@ -14,53 +14,217 @@ public class RegisterPage {
 	public RegisterPage(WebDriver driver) {
 
 		this.driver = driver;
-	    PageFactory.initElements(driver, this);
+		PageFactory.initElements(driver, this);
 	}
-	
+
 	// locating header of register page
-	@FindBy(xpath = "//div[@class = 'main-header']") 
+	@FindBy(xpath = "//div[@class = 'main-header']")
 	private WebElement registerPage;
-	
-	// locating text on register form 
-	@FindBy(xpath = "//div[@class='register-wrapper']//h4[text()='Register to Book Store']") 
+
+	// locating text on register form
+	@FindBy(xpath = "//div[@class='register-wrapper']//h4[text()='Register to Book Store']")
 	private WebElement registerFormText;
-	
-	//first name field
-	@FindBys({@FindBy(id="firstname"),@FindBy(tagName="input")}) 
+
+	// first name field
+	@FindBys({ @FindBy(id = "firstname"), @FindBy(tagName = "input") })
 	private WebElement firstName;
-	
-	//last name field
-	@FindBys({@FindBy(id="lastname"),@FindBy(tagName="input")}) 
+
+	// last name field
+	@FindBys({ @FindBy(id = "lastname"), @FindBy(tagName = "input") })
 	private WebElement lastName;
-	
-	//user name field
-	@FindBys({@FindBy(id="userName"),@FindBy(tagName="input")}) 
+
+	// user name field
+	@FindBys({ @FindBy(id = "userName"), @FindBy(tagName = "input") })
 	private WebElement userName;
-	
-	//password field
-	@FindBys({@FindBy(id="password"),@FindBy(tagName="input")}) 
+
+	// password field
+	@FindBys({ @FindBy(id = "password"), @FindBy(tagName = "input") })
 	private WebElement password;
-	
-	//locating register button
-	@FindBys({@FindBy(id="register"),@FindBy(tagName="button")}) 
+
+	// locating register button
+	@FindBys({ @FindBy(id = "register"), @FindBy(tagName = "button") })
 	private WebElement registerButton;
-	
-	//locating back to login button
-	@FindBys({@FindBy(id="gotologin"),@FindBy(tagName="button")}) 
+
+	// locating back to login button
+	@FindBys({ @FindBy(id = "gotologin"), @FindBy(tagName = "button") })
 	private WebElement backToLoginButton;
-	
-	//locating captcha box
-	@FindBy(xpath = "//div[@id='rc-anchor-cntainer']//div[@class='recaptcha-checkbox-border']") 
+
+	// locating captcha box
+	@FindBy(xpath = "//div[@id='rc-anchor-cntainer']//div[@class='recaptcha-checkbox-border']")
 	private WebElement captchaCheckBox;
-	
-	
-	//verify register loading or not 
-	public void verifyRegisterPage(){
-		
-		String registerPageTitle ="";
+
+	/**
+	 * @return the registerPage
+	 */
+	public WebElement getRegisterPage() {
+		return registerPage;
+	}
+
+	/**
+	 * @return the registerFormText
+	 */
+	public WebElement getRegisterFormText() {
+		return registerFormText;
+	}
+
+	/**
+	 * @return the firstName
+	 */
+	public WebElement getFirstName() {
+		return firstName;
+	}
+
+	/**
+	 * @return the lastName
+	 */
+	public WebElement getLastName() {
+		return lastName;
+	}
+
+	/**
+	 * @return the userName
+	 */
+	public WebElement getUserName() {
+		return userName;
+	}
+
+	/**
+	 * @return the password
+	 */
+	public WebElement getPassword() {
+		return password;
+	}
+
+	/**
+	 * @return the registerButton
+	 */
+	public WebElement getRegisterButton() {
+		return registerButton;
+	}
+
+	/**
+	 * @return the backToLoginButton
+	 */
+	public WebElement getBackToLoginButton() {
+		return backToLoginButton;
+	}
+
+	/**
+	 * @return the captchaCheckBox
+	 */
+	public WebElement getCaptchaCheckBox() {
+		return captchaCheckBox;
+	}
+
+	// verify register loading or not
+	public void verifyRegisterPage() {
+
+		String registerPageTitle = "";
 		registerPageTitle = registerPage.getText();
-		
+
 		Assert.assertEquals("Register", registerPageTitle, "Register Page did not load successfully");
+	}
+
+	// click on register button
+	public void clickOnRegister() {
+
+		registerButton.click();
+	}
+
+	// click on back to login button
+	public void clickOnBackToLogin() {
+
+		backToLoginButton.click();
+	}
+
+	// click on captcha check box
+	public void clickOnCaptcha() {
+
+		captchaCheckBox.click();
+	}
+
+	// method to fill all the fields of registration form
+	public void fillRegistrationForm(String firstname, String lastname, String username, String password) {
+
+		firstName.sendKeys(firstname);
+		lastName.sendKeys(lastname);
+		userName.sendKeys(username);
+		this.password.sendKeys(password);
+	}
+
+	// method to fill first name
+	public void fillFirstName(String firstname) {
+
+		firstName.sendKeys(firstname);
+
+	}
+
+	// method to fill last name
+	public void fillLastName(String lastname) {
+
+		lastName.sendKeys(lastname);
+
+	}
+
+	// method to fill user name
+	public void fillUserName(String username) {
+
+		userName.sendKeys(username);
+	}
+
+	// method to fill password
+	public void fillPassword(String password) {
+
+		this.password.sendKeys(password);
+
+	}
+
+	// verify first name mandatory indication
+	public void verifyFirstNameMandatoryIndication() {
+
+		// get class attribute from input field
+		String classAttribute = firstName.getAttribute("class");
+
+		// apply assertion to verify
+		Assert.assertTrue(classAttribute.contains("is-invalid"),
+				"First Name field not highlighted with mandatory message");
+
+	}
+
+	// verify last name mandatory indication
+	public void verifyLastNameMandatoryIndication() {
+
+		// get class attribute from input field
+		String classAttribute = lastName.getAttribute("class");
+
+		// apply assertion to verify
+		Assert.assertTrue(classAttribute.contains("is-invalid"),
+				"Last Name field not highlighted with mandatory message");
+
+	}
+
+	// verify user name mandatory indication
+	public void verifyUserNameMandatoryIndication() {
+
+		// get class attribute from input field
+		String classAttribute = userName.getAttribute("class");
+
+		// apply assertion to verify
+		Assert.assertTrue(classAttribute.contains("is-invalid"),
+				"User Name field not highlighted with mandatory message");
+
+	}
+
+	// verify password mandatory indication
+	public void verifyPasswordMandatoryIndication() {
+
+		// get class attribute from input field
+		String classAttribute = password.getAttribute("class");
+
+		// apply assertion to verify
+		Assert.assertTrue(classAttribute.contains("is-invalid"),
+				"Password field not highlighted with mandatory message");
+
 	}
 
 }
